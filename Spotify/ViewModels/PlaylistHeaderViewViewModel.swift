ec194c7b52e054b119044e0baaec4813db83cd5f
haju <haju0321@gmail.com>
2021-09-16 00:40:39 +0900

    챕터 13, 플레이버튼 제작, 재생목록 자세히 보기 구현

---
+++ Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by haju Kim on 2021/09/15.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
